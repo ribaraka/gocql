9f9c1850e08ae883e830d1592e9cad3e0b0ba69c
Bychkov <stanislav.bychkov67@gmail.com>
2024-06-25 17:58:38 +0300

    bash script: update cass config

--- main_test.go
+++ main_test.go
@@ -60,22 +60,23 @@ func TestCassandraWithWaitStrategy2(t *testing.T) {
 				"CASSANDRA_ENDPOINT_SNITCH": "GossipingPropertyFileSnitch",
 				"CASSANDRA_DC":              "datacenter1",
 			},
-			//WaitingFor: wait.ForAll(
-			//	wait.ForListeningPort(port),
-			//	wait.ForLog("Configuration location: file:/etc/cassandra/cassandra.yaml"),
-			//wait.ForExec([]string{"cqlsh", "-e", "SELECT bootstrapped FROM system.local"}).WithResponseMatcher(func(body io.Reader) bool {
-			//	data, _ := io.ReadAll(body)
-			//	return strings.Contains(string(data), "COMPLETED")
-			//}),
-			//),
 			Files: []testcontainers.ContainerFile{
 				{
-					HostFilePath:      "cass_init_origin.sh",
-					ContainerFilePath: "/cass_init_origin.sh",
+					HostFilePath:      "working.sh",
+					ContainerFilePath: "/working.sh",
+					FileMode:          0o700,
+				},
+				{
+					HostFilePath:      "./testdata/pki/.keystore",
+					ContainerFilePath: "testdata/.keystore",
+					FileMode:          0o700,
+				},
+				{
+					HostFilePath:      "./testdata/pki/.truststore",
+					ContainerFilePath: "testdata/.truststore",
 					FileMode:          0o700,
 				},
 			},
-			//Cmd: []string{"bash", "/waitForcass_init2.sh"},
 		},
 		Started: true,
 	})
@@ -88,40 +89,39 @@ func TestCassandraWithWaitStrategy2(t *testing.T) {
 		}
 	}()
 
-	fmt.Println("sleep")
-	//time.Sleep(10 * time.Second)
-	//code, _, err := container.Exec(ctx, []string{"bash", "./cass_init_origin.sh"})
-	code, _, err := container.Exec(ctx, []string{"bash", "./cass_init_origin.sh"})
-
+	//time.Sleep(1500 * time.Millisecond)
+	code, _, err := container.Exec(ctx, []string{"bash", "./working.sh"})
 	require.NoError(t, err)
 	require.Zero(t, code)
-	// Give some time to the wait script to catch the hello script being created
-
-	//err = wait.ForLog("Cassandra configuration modified successfully.").WithStartupTimeout(20*time.Second).WaitUntilReady(ctx, container)
-	//require.NoError(t, err)
 
 	cmd := exec.Command("docker", "exec", container.GetContainerID(), "cat", "/etc/cassandra/cassandra.yaml")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("Failed to check write_request_timeout_in_ms: %v\n", err)
 	}
-	//
+
 	fmt.Printf("CombinedOutput output: %s\n", string(output))
 
+	//cmd := exec.Command("docker", "exec", container.GetContainerID(), "ls", "-a", "/testdata")
+	//output, err := cmd.CombinedOutput()
+	//if err != nil {
+	//	log.Fatalf("Failed to check write_request_timeout_in_ms: %v\n", err)
+	//}
+	//
+	//fmt.Printf("CombinedOutput output: %s\n", string(output))
+	//
 	host, err := container.Host(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	port, err := container.MappedPort(ctx, port)
+	mappedPort, err := container.MappedPort(ctx, port)
 	if err != nil {
 		t.Fatal(err)
 	}
-	//
-	fmt.Println("host and port", host, port)
-	//
-	containerHost := host + ":" + port.Port()
-	//
+
+	containerHost := host + ":" + mappedPort.Port()
+
 	cluster := NewCluster(containerHost)
 
 	session, err := createContaienrSessione(cluster, 10, 2*time.Second)
@@ -133,44 +133,13 @@ func TestCassandraWithWaitStrategy2(t *testing.T) {
 	var result string
 	err = session.Query("SELECT cluster_name FROM system.local").Scan(&result)
 	require.NoError(t, err)
-	fmt.Println("CLUSTER NAME IS ", result)
+	//fmt.Println("CLUSTER NAME IS ", result)
 	assert.Equal(t, "MyCluster", result)
+
 }
 
 func createContaienrSessione(cluster *ClusterConfig, retries int, delay time.Duration) (*Session, error) {
 	for i := 0; i < retries; i++ {
-
-		//host, err := c.Host(ctx)
-		//if err != nil {
-		//	return nil, fmt.Errorf("unable to get host %s", err)
-		//}
-		//
-		//newpport, err := c.MappedPort(ctx, port)
-		//if err != nil {
-		//	return nil, fmt.Errorf("unable to get  mapped port %s", err)
-		//}
-
-		//fmt.Println("newpport.Port()", newpport.Port())
-		//
-		//result, err := IncrementString(newpport.Port())
-		//if err != nil {
-		//	return nil, fmt.Errorf("unable to increment a string mapped port %s", err)
-		//
-		//}
-
-		//newpport = nat.Port(newpport.Int() + 1))
-		//containerHost := host + ":" + newpport.Port()
-		//containerHost := host + ":" + result
-
-		//containerHost, err := c.ConnectionHost(ctx)
-		//if err != nil {
-		//	log.Fatalf("Failed to get container host: %s", err)
-		//}
-
-		//fmt.Println("ConnectionHost", containerHost)
-
-		//cluster := NewCluster(containerHost)
-
 		session, err := cluster.CreateSession()
 		if err == nil {
 			return session, nil
@@ -178,5 +147,6 @@ func createContaienrSessione(cluster *ClusterConfig, retries int, delay time.Dur
 		log.Printf("Attempt %d: Unable to create session: %v", i+1, err)
 		time.Sleep(delay)
 	}
+
 	return nil, fmt.Errorf("unable to create session after %d retries", retries)
 }
